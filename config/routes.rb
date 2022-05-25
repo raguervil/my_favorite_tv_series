330b34fd004fb2492ab2b9caa4ccdfba016ca3b2
Reinaldo <r.guerra.villagra@gmail.com>
2022-05-25 19:43:13 -0400

    modificaciones proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-    get '/tv_shows', to: 'tv_shows#index'
-    get '/welcome', to: 'pages#welcome' 
-    post '/tv_shows', to: 'tv_shows#create'
-    root 'pages#welcome'
+  resources :tv_shows
+  get '/welcome', to: 'pages#welcome' 
+  root 'pages#welcome'
 end
